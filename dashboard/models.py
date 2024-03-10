9d1037964ffc108b30d30a7cedada3904339e6a0
Tyson Burghardt <tyson@uncountable.org>
2024-03-10 12:01:00 -0400

    models

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Patient(models.Model):
+    given = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    dob = models.DateField()
+    mrn = models.CharField(max_length=20)
+
+class Recording(models.Model):
+    patient = models.ForeignField("Patient", on_delete=models.CASCADE)
+    start_epoch = DateTimeField()
+    comments = models.CharField(max_length=250)
+
